e4ec87b6249c789678e12e47344380a837e28893
2youngjun <86882798+2youngjun@users.noreply.github.com>
2022-06-27 11:32:33 +0900

    Initial Commit

---
+++ CatchTable_Clone/CatchTable_Clone/CatchTable_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatchTable_CloneApp.swift
+//  CatchTable_Clone
+//
+//  Created by 이영준 on 2022/06/27.
+//
+
+import SwiftUI
+
+@main
+struct CatchTable_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CatchTable_Clone/CatchTable_Clone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CatchTable_Clone
+//
+//  Created by 이영준 on 2022/06/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
